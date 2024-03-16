0af4dd75f2aa093cc54ba1c02c3ff448a7a3287c
JoshuaSitumeang <160564706+JoshuaSitumeang@users.noreply.github.com>
2024-03-16 10:07:45 +0000

    Assignment 4b Submission

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
 # Type your code below
+std_pos = stdform.find('x10^')
+mantissa = stdform[:std_pos]
+# print(mantissa)
+exponent = stdform[std_pos+4:]
+# print(exponent)
+result = mantissa + 'E' + exponent
+print(f'This number in E notation is {result}.')
